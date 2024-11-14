2f016a62f50dae780da06d09a7bb86c81bf54e46
sum <tinafongfong@gmail.com>
2024-11-15 00:13:55 +0100

    1-10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-// Code here!
+const h3 = document.querySelectorAll("h3");
+
+h3[0].innerText = "Potato";
+
+const anchor = document.querySelectorAll("a");
+anchor[0].innerText = "Start";
+anchor[2].innerText = "Mail Us";
+
+const paragraph = document.querySelectorAll("p");
+paragraph[1].innerText = "This hoodie is on FIRE.";
+
+const button = document.querySelectorAll("button");
+button[1].innerText = "Roast";
+button[1].style.backgroundColor = "maroon";
+
+const figure = document.querySelectorAll("figure");
+figure[1].style.backgroundColor = "gray";
+
+paragraph[3].innerText = "Kalkstensvagen 1, Lund";
+
+paragraph.forEach((item) => {
+  item.style.color = "pink";
+});
+
+button.forEach((button) => {
+  button.innerText = "Add to Cart";
+});
+
+anchor[0].classList.add("active");
+
+const image = document.querySelectorAll("image");
