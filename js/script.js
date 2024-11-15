06e3ff1364b25adf06096146658aedcc3baa8685
sum <tinafongfong@gmail.com>
2024-11-15 12:13:08 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -28,4 +28,38 @@ button.forEach((button) => {
 
 anchor[0].classList.add("active");
 
-const image = document.querySelectorAll("image");
+const image = document.querySelectorAll("img");
+image[0].classList.remove("logo");
+
+const newAnchor = document.createElement("a");
+const navBar = document.querySelector("nav");
+
+newAnchor.innerText = "Subscribe";
+
+navBar.insertAdjacentElement("afterbegin", newAnchor);
+
+const newArticle = document.createElement("article");
+const mainElement = document.querySelector("main");
+
+mainElement.insertAdjacentElement("beforeend", newArticle);
+
+newArticle.innerHTML = `<figure><img src="img/hoodie-forrest.png" alt="hoodie"></figure>
+            <h2>Sinus Hoodie</h2>
+            <h3>Forrest</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolores.</p>
+            <button>Buy</button>`;
+
+image[0].addEventListener("click", () => {
+  console.log("found you!");
+});
+
+const article = document.querySelectorAll("article");
+
+articlesArray = [article[0], article[1], article[2]];
+console.log(articlesArray);
+
+for (let i = 0; i < articlesArray.length; i++) {
+  article[i].addEventListener("click", () => {
+    console.log(`Hi, I am article ${h3[i].innerText}.`);
+  });
+}
